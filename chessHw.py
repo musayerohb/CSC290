f3b11bfd87bb4b63141ac87510df44a9c9ee9798
Musayeroh Bah <mbah@smith.edu>
2024-10-22 19:14:05 -0400

    Co-authored-by: mgilliom <mgilliom@users.noreply.github.com> Co-authored-by: amartinez-69 <amartinez-69@users.noreply.github.com>

--- chessHw.py
+++ chessHw.py
@@ -5,16 +5,109 @@ import datetime
 
 # board = chess.Board()
 
-def computerChooseMove():
-  pass
+def computerChooseMove(board):
+  legal_moves = list(board.legal_moves)
+  capture_moves = []
+  for move in legal_moves:
+    if board.is_capture(move):
+      capture_moves.append(move)
+  
+  if not capture_moves: #if list is empty
+    random_move = legal_moves[randint(0, len(legal_moves)-1)]
+  else:
+    random_move = capture_moves[randint(0, len(capture_moves)-1)]
+  return random_move
 
-def startWhite():
-  pass
 
-def startBlack():
-  pass
+  #If one or more moves are available which capture an enemy piece, 
+  # your program should always choose one of the capture moves.  Otherwise, program should 
+  # randomly choose a move from among all of the available valid moves. 
+  
+
+def computerWhite(board):
+  #do the first move, f2f4
+  # first_move = chess.Move.from_uci("f2f4")
+
+  while board.outcome() is None:
+
+    first_move = computerChooseMove(board)
+    print("computer move (white): ", first_move)
+    board.push(first_move)
+    print(board.fen())
+    print(board)
+    if board.is_checkmate():
+      print("Checkmate!")
+    if board.outcome() is not None:
+      break
 
 
+    #Black makes move
+    black_first_move = chess.Move.from_uci(input('Black: '))
+    while black_first_move not in board.legal_moves:
+      black_first_move = chess.Move.from_uci(input('Move not legal. Pls enter new move: '))
+    board.push(black_first_move)
+    print("New FEN Position: " + board.fen())
+    print(board)
+
+  
+  return board
+
+  
+
+  # computerChooseMove(board.fen(), board)
+
+def computerBlack(board):
+  while (board.outcome()) is not None:
+
+    #white makes first move
+    white_first_move = chess.Move.from_uci(input('White: '))
+    while white_first_move not in board.legal_moves:
+      white_first_move = chess.Move.from_uci(input('Move not legal. Pls enter new move: '))
+    board.push(white_first_move)
+    print("New FEN Position: " + board.fen())
+    print(board)
+    if board.is_checkmate():
+      print("Checkmate!")
+    if board.outcome():
+      break
+
+    first_move = computerChooseMove(board)
+    print("computer move (black): ", first_move)
+    board.push(first_move)
+    print(board.fen())
+    print(board)
+
+  return board
+
+
+def botsPlay(board):
+  #do the first move, f2f4
+  # first_move = chess.Move.from_uci("f2f4")
+
+  while not board.outcome():
+
+    first_move = computerChooseMove(board)
+    print("computer move (white): ", first_move)
+    board.push(first_move)
+    print(board.fen())
+    print(board)
+    if board.is_checkmate():
+      print("Checkmate!")
+    if board.outcome():
+      break
+
+
+    #Black makes move
+    black_first_move = computerChooseMove(board)
+    while black_first_move not in board.legal_moves:
+      black_first_move = chess.Move.from_uci(input('Move not legal. Pls enter new move: '))
+    board.push(black_first_move)
+    print("New FEN Position: " + board.fen())
+    print(board)
+  
+  
+
+  return
 
 def main():
   print('=====================================================\nCS 290 Chess Bot Version 0.1\n=====================================================')
@@ -23,8 +116,24 @@ def main():
   print('Time: ', x)
   computer_player = input('Computer Player (w=white/b=black): ')
   starting_FEN = input('Starting FEN position? (enter for standard starting position): ')
-  board = chess.Board()
+  if starting_FEN == '':
+    board = chess.Board()
+  else:
+    board = chess.Board(starting_FEN)
   print(board)
+  print(board.fen())
+  if computer_player == 'w':
+    computerWhite(board)
+  elif computer_player == 'b':
+    computerBlack(board)
+  elif computer_player == 'bots':
+    botsPlay(board)
+
+  if board.is_stalemate():
+    print("It's a tie!")
+  elif board.is_insufficient_material():
+    print("Neither side has sufficient enough material to win!")
+  
   #in a loop:
   #white move, new fen, black moves, new fen.
   #maybe after ea move, should check if anyone has won or sth? and before ea computer turn, the computer needs a list of moves (board.legal_moves)
